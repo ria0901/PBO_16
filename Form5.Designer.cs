fd2245ec8534032ccf147d9d293db4c7f4a4e0fb
ria0901 <80446413+ria0901@users.noreply.github.com>
2022-06-25 09:35:02 +0700

    Add files via upload

---
+++ Form5.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace GajiKu
+{
+    partial class Form5
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ImageAlign = System.Drawing.ContentAlignment.TopLeft;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(152, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Masukan ID gaji";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(196, 14);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(187, 26);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(18, 74);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(110, 49);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Cetak";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 155);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowHeadersWidth = 62;
+            this.dataGridView1.RowTemplate.Height = 28;
+            this.dataGridView1.Size = new System.Drawing.Size(959, 233);
+            this.dataGridView1.TabIndex = 3;
+            // 
+            // Form5
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(983, 400);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form5";
+            this.Text = "Cetak Slip Gaji";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
---
+++ Form5.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Npgsql;
+
+namespace GajiKu
+{
+    public partial class Form5 : Form
+    {
+        private string id = "";
+        private int intRow = 0;
+        public Form5()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            loadData("");
+        }
+
+
+        private void addParameters(string str)
+        {
+            
+            CRUD.cmd.Parameters.Clear();
+            CRUD.cmd.Parameters.AddWithValue("@id", textBox1.Text);
+            
+
+            if (str == "Cari" && !string.IsNullOrEmpty(this.id))
+            {
+                CRUD.cmd.Parameters.AddWithValue("id", this.id);
+            }
+        }
+
+
+        private void loadData(string keyword)
+        {
+            CRUD.sql = "SELECT * FROM slipgaji WHERE id_gaji = " + textBox1.Text;
+
+            string strKeyword = string.Format("%{0}%", keyword);
+
+            CRUD.cmd = new NpgsqlCommand(CRUD.sql, CRUD.con);
+            CRUD.cmd.Parameters.Clear();
+            CRUD.cmd.Parameters.AddWithValue("keyword", strKeyword);
+
+            DataTable dt = CRUD.PerformCRUD(CRUD.cmd);
+
+            if (dt.Rows.Count > 0)
+            {
+                intRow = Convert.ToInt32(dt.Rows.Count.ToString());
+            }
+            else
+            {
+                intRow = 0;
+            }
+
+            DataGridView dgv1 = dataGridView1;
+
+            dgv1.MultiSelect = false;
+            dgv1.AutoGenerateColumns = true;
+            dgv1.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+
+            dgv1.DataSource = dt;
+
+            dgv1.Columns[0].HeaderText = "ID";
+            dgv1.Columns[1].HeaderText = "Tanggal Periode";
+            dgv1.Columns[2].HeaderText = "Gaji Pokok";
+            dgv1.Columns[3].HeaderText = "Tunjangan";
+            dgv1.Columns[4].HeaderText = "THR";
+            dgv1.Columns[5].HeaderText = "Lemburan";
+
+
+            dgv1.Columns[0].Width = 50;
+            dgv1.Columns[1].Width = 100;
+            dgv1.Columns[2].Width = 100;
+            dgv1.Columns[3].Width = 100;
+            dgv1.Columns[4].Width = 100;
+            dgv1.Columns[5].Width = 100;
+
+
+        }
+    }
+}
